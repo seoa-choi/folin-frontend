528ce8dbf6c043eabc2e4f1558f66c25e8defc0f
seoa-choi <[choichoi120@naver.com](mailto:dalhoya@naver.com)>
2025-07-01 10:53:22 +0900

    feat: 시리즈(리스트 제외), 아티클 상세 간략하게 처리

--- app/components/article/ArticleDetail.tsx
+++ app/components/article/ArticleDetail.tsx
@@ -4,6 +4,7 @@ import { useQuery } from '@tanstack/react-query';
 import { createColorAssigner } from '@/app/_lib/colorUtils';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useState } from 'react';
 
 type ArticleData = {
   contents_id: number;
@@ -28,15 +29,37 @@ type ArticleData = {
 };
 
 const icons = [
-  { icon: '/images/ico_review_default.svg', alt: 'comment' },
-  { icon: '/images/like.png', alt: 'like' },
-  { icon: '/images/bookmark.png', alt: 'scrap' },
-  { icon: '/images/share.png', alt: 'share' },
-  { icon: '', alt: '' },
-  { icon: '/images/gift.png', alt: 'gift' },
+  {
+    icon: '/images/ico_review_default.svg',
+    alt: 'comment',
+    activeIcon: '/images/ico_review_active.svg',
+  },
+  {
+    icon: '/images/like.png',
+    alt: 'like',
+    activeIcon: '/images/like_active.png',
+  },
+  {
+    icon: '/images/bookmark.png',
+    alt: 'scrap',
+    activeIcon: '/images/bookmark_active.png',
+  },
+  {
+    icon: '/images/share.png',
+    alt: 'share',
+    activeIcon: '/images/share_active.png',
+  },
+  { icon: '', alt: '', activeIcon: '' },
+  {
+    icon: '/images/gift.png',
+    alt: 'gift',
+    activeIcon: '/images/gift_active.png',
+  },
 ];
 
 export default function ArticleDetail({ articleId }: { articleId: string }) {
+  // 아이콘 마우스오버 액티브
+  const [hoverIndex, setHoverIndex] = useState<number | null>(null);
   const { data, isLoading, isError } = useQuery<ArticleData[]>({
     queryKey: ['article', articleId],
     queryFn: async () => {
@@ -88,59 +111,97 @@ export default function ArticleDetail({ articleId }: { articleId: string }) {
   const filteredId = indexedArticleSeries?.filter(
     (item) => String(item.contents_id) === articleId
   );
-  console.log(filteredId);
+  // console.log(filteredId);
+
+  function handleMouseHover(idx: number | null) {
+    setHoverIndex(idx);
+  }
 
   return (
     <main className="pt-[52px] px-[24px] max-w-[1248px] mx-auto max-sm:pt-[56px] max-sm:px-[8px]">
-      <div className="w-[588px] pt-[64px] mx-auto">
-        <div>
-          {filteredId?.map((item, i) => (
-            <div key={i}>
-              <div className="flex gap-[2px]">
-                <div
-                  className={`border rounded-[6px] py-[6px] px-[8px] text-[12px] font-bold ${item.seriesColor.bg} ${item.seriesColor.bd}`}
-                >
-                  {item.series_title}
-                </div>
-                <div
-                  className={`bg-transprent border rounded-[6px] py-[6px] px-[8px] text-[12px] font-bold ${item.seriesColor.bd}`}
-                >
-                  {item.seriesIndex + '화'}
-                </div>
+      {filteredId?.map((item, i) => (
+        <div key={i} className="w-[588px] pt-[64px] mx-auto">
+          <div className="mb-[64px]">
+            <div className="flex gap-[2px]">
+              <div
+                className={`border rounded-[6px] py-[6px] px-[8px] text-[12px] font-bold ${item.seriesColor.bg} ${item.seriesColor.bd}`}
+              >
+                {item.series_title}
               </div>
-              <h3 className="text-[28px] font-bold leading-[36px]">
-                {item.sub_title}
-              </h3>
-              <div className="flex justify-end items-center gap-[8px] mt-[26px] mb-[28px]">
-                {icons.map((ite) => (
-                  <button
-                    key={ite.alt}
-                    type="button"
-                    className="group nth-[5]:border-r nth-[5]:h-[16px]"
-                  >
-                    {ite.icon && (
-                      <Image
-                        src={ite.icon}
-                        alt={ite.alt}
-                        width={24}
-                        height={24}
-                      />
-                    )}
-                  </button>
-                ))}
-              </div>
-              <div>
-                {item.linkers.map((linker) => (
-                  <Link href="" key={linker.linker_id} className="block">
-                    <strong className="font-bold">{linker.author}</strong>
-                    <span>{linker.affiliation}</span>
-                  </Link>
-                ))}
+              <div
+                className={`bg-transprent border rounded-[6px] py-[6px] px-[8px] text-[12px] font-bold ${item.seriesColor.bd}`}
+              >
+                {item.seriesIndex + '화'}
               </div>
             </div>
-          ))}
+            <h3 className="text-[28px] font-bold leading-[36px]">
+              {item.sub_title}
+            </h3>
+            <div className="flex justify-end items-center gap-[8px] mt-[26px] mb-[28px]">
+              {icons.map((ite, i) => (
+                <button
+                  key={ite.alt}
+                  type="button"
+                  className="group nth-[5]:border-r nth-[5]:h-[16px]"
+                  onMouseEnter={() => handleMouseHover(i)}
+                  onMouseLeave={() => handleMouseHover(null)}
+                >
+                  {ite.icon && (
+                    <Image
+                      src={hoverIndex === i ? ite.activeIcon : ite.icon}
+                      alt={ite.alt}
+                      width={24}
+                      height={24}
+                    />
+                  )}
+                </button>
+              ))}
+            </div>
+            <div className="flex flex-col gap-y-[8px]">
+              {item.linkers.map((linker) => (
+                <Link href="" key={linker.linker_id} className="flex gap-[4px]">
+                  <Image
+                    src={`http://localhost:3001/${linker.image_url}`}
+                    alt={linker.author}
+                    width={32}
+                    height={32}
+                    className="rounded-[50%]"
+                  />
+                  <strong className="font-bold">{linker.author}</strong>
+                  <span>{linker.affiliation}</span>
+                </Link>
+              ))}
+            </div>
+          </div>
+          {item.detail.sentence1 && (
+            <div className="p-[24px_0] mb-[64px] border-t border-b border-[#00d48d]">
+              <h3 className="mb-[8px] text-[13px] font-bold">3줄 요약</h3>
+              <ul className="list-disc flex flex-col gap-[12px] ml-[13px]">
+                <li>{item.detail.sentence1}</li>
+                <li>{item.detail.sentence2}</li>
+                <li>{item.detail.sentence3}</li>
+              </ul>
+            </div>
+          )}
+          <div className="mb-[40px] text-center px-[8px]">
+            <h3 className="text-[24px]">
+              {!item.detail.h3 ? item.sub_title : item.detail.h3}
+            </h3>
+            <figure className="mt-[36px]">
+              <Image
+                src={`http://localhost:3001/${item.img_url}`}
+                alt={!item.detail.p ? item.sub_title : item.detail.p}
+                width={572}
+                height={429}
+                priority
+              />
+              <figcaption className="mt-[8px] text-[13px] text-left">
+                {!item.detail.p ? item.series_title : item.detail.p}
+              </figcaption>
+            </figure>
+          </div>
         </div>
-      </div>
+      ))}
     </main>
   );
 }
