b66e28c2660af9e3bb4ecaa30e9ea5270ddc88c4
seoa-choi <[choichoi120@naver.com]>
2025-07-09 15:52:36 +0900

    fix: next.config.ts 이미지 도메인 수정

--- app/components/home/SeriesInside.tsx
+++ app/components/home/SeriesInside.tsx
@@ -84,12 +84,6 @@ export default function SeriesInside() {
             >
               {item.items.map((int, idx) => {
                 const reverseIndex = item.items.length - idx;
-                console.log(
-                  new URL(
-                    int.img_url,
-                    process.env.NEXT_PUBLIC_API_URL
-                  ).toString()
-                );
 
                 return (
                   <SwiperSlide key={int.contents_id}>
