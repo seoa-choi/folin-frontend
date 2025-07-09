75e5761475e887845273248d2779981bd3fcef61
seoa-choi <[choichoi120@naver.com]>
2025-07-09 14:48:48 +0900

    feat: 버셀 배포 에러 메세지 수정 8

--- app/(main)/interleaving/page.tsx
+++ app/(main)/interleaving/page.tsx
@@ -4,10 +4,10 @@
 export default function InterLeaving() {
   return (
     <main className="p-[30px]">
-      // <h2 className="text-[26px] font-bold">인터리빙 페이지</h2>
-      {/* //       <ClientOne>
-//         <ServerOne />
-//       </ClientOne> */}
+      <h2 className="text-[26px] font-bold">인터리빙 페이지</h2>
+      {/*        <ClientOne>
+        <ServerOne />
+       </ClientOne> */}
     </main>
   );
 }
